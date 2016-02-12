1d2699e11aa49e1901efc98a8e878f7be63cd0bc
oerciyes <oerciyes@hotmail.com>
2016-02-12 01:11:23 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1,40 +1,19 @@
-library(shiny)
-library(car)
+library(shiny) 
 
-shinyServer(function(input, output) {
-  
-  values <- reactiveValues()
-  
-  observe({
-    input$Calculate
-    values$expected <- isolate({
-      (input$optimistic+input$pessimistic+input$likely*4)/6
-      
-    })
-  })
+Expected<-function(optimistic,pessimistic,likely) {(optimistic+pessimistic+(4*likely))/6}
 
+diagnostic_f<-function(optimistic,pessimistic,likely){
+  Expected_value<-(optimistic+pessimistic+(4*likely))/6
+}
 
-
-# Display values entered
-  output$optimistic <- renderText({
-    input$Calculate
-    paste("Your optimistic time estimate [in hours] :", isolate(input$optimistic))
-  })
-  
-  output$pesimistic <- renderText({
-    input$Calculate
-    paste("Your pessimistic time estimate [in hours] : ", isolate(input$pessimistic))
-  })
-  
-  output$likely <- renderText({
-    input$Calculate
-    paste("Your likely time estimate [in hours] : ", isolate(input$likely))
-  })
-
-  output$text_expected <- renderText({
-    if(input$Calculate == 0) ""
-    Else
-      
-      paste("Expected Time is:", values$expected)
-  })
-
+shinyServer(
+  function(input, output) {
+    
+    output$inputoptimisticvalue <- renderPrint({input$optimistic})
+    output$inputpessimisticvalue <- renderPrint({input$pessimistic})
+    output$inputlikelyvalue <- renderPrint({input$likely})
+    output$estimation <- renderPrint({Expected(input$optimistic,input$pessimistic,input$likely)})
+    output$diagnostic <- renderPrint({diagnostic_f(input$optimistic,input$pessimistic,input$likely)})
+    
+  } 
+)
