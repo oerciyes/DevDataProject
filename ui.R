5c77c4726c68d69ef0c81d510bcadf664e3f53d6
oerciyes <oerciyes@hotmail.com>
2016-02-12 01:10:33 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,41 +1,28 @@
-library(shiny)
-shinyUI(fluidPage(
-  titlePanel("Expected Time Calculator"),
-  
-  sidebarLayout(
-    sidebarPanel( 
-      
-      helpText("Enter your optimistic, pessimistic and likely time estimates for the project to calculate the Expected Time"),            
-      br(),           
-      numericInput("optimistic",label = h4("Your Optimistic Time Estimate (hours)"),min = 1, max = 300,1), 
-      br(),
-      numericInput("pessimistic",label = h4("Your Pessimistic Time Estimate (hours)"),min = 1, max = 300,1),
-      br(),
-      numericInput("likely",label = h4("Your Likely Time Estimate (hours)"),min = 1, max = 300,1), 
-      br(),
-      actionButton("Calculate", label = "submit")    
-  
-         
-    ),
+library(shiny) 
+shinyUI(
+  pageWithSidebar(
+    # Application title
+    headerPanel("EXPECTED TIME CALCULATOR (Systems Development Tool)"),
+    
+    sidebarPanel(
+      numericInput('optimistic', 'Insert your optimistic time estimate (h)', 0, min = 1, max = 300, step = 1) ,
+      numericInput('pessimistic', 'Insert your pessimistic time estimate (h)', 0, min = 1, max = 300, step = 1),
+      numericInput('likely', 'Insert your realistic time estimate (h)', 0, min = 1, max = 300, step = 1),
+      submitButton('Submit')
+    ), 
     mainPanel(
-      tabsetPanel(
-        tabPanel("Expected Time Calculation",
-          p(h4("Your Input:")),
-          textOutput("text_optimistic"),
-          textOutput("text_pessimistic"),
-          textOutput("text_likely"),
-          p(h4("Expected Time:")),
-          textOutput("text_expected"),
-                 ),
-        tabPanel(
-          "Documentation",
-          p(h4("Expected Time Calculator:")),
-          br(),
-          helpText("This application calculates the expected time for a task in a Systems Development project, for given optimistic, pessimistic, likely time estimates. Please enter values for your three estimated scenarios in the form"),
-                 )
-        
-        )
       
-      )
-  )
-))
+      h4('You have entered the following values:'), 
+      p('optimistic:'), verbatimTextOutput("inputoptimisticvalue"),
+      p('pessimistic:'), verbatimTextOutput("inputpessimisticvalue"),
+      p('likely:'), verbatimTextOutput("inputlikelyvalue"),
+      h4('The expected time is:'),
+      verbatimTextOutput("estimation"),
+      p('EXPECTED TIME IS CALCULATED ABOVE'),strong(verbatimTextOutput("diagnostic"))
+      
+      
+      
+    )
+    
+  )   
+)
