fae3387973e72afba228cfa2a18760338e0e3d36
oerciyes <oerciyes@hotmail.com>
2016-02-11 23:07:15 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,50 +1,37 @@
 library(shiny)
 shinyUI(fluidPage(
-  titlePanel("Obesity Calculator"),
+  titlePanel("Expected Time Calculator"),
   
   sidebarLayout(
     sidebarPanel( 
       
-      helpText("Enter your height and weight to calculate your BMI(Body Mass Index)"),            
+      helpText("Enter your optimistic, pessimistic and likely time estimates for the project to calculate the Expected Time"),            
       br(),           
-      numericInput("num_height",label = h4("How tall are you? (centimeters)"),min = 5, max = 500,0), #,value = 150
+      numericInput("optimistic",label = h4("Your Optimistic Time Estimate (hours)"),min = 1, max = 300,1), 
       br(),
-      numericInput("num_weight",label = h4("How much do you weigh? (kilograms)"),min = 5, max = 500,0), #, value = 50
+      numericInput("pessimistic",label = h4("Your Pessimistic Time Estimate (hours)"),min = 1, max = 300,1),
       br(),
-      
-      br(),   
+      numericInput("likely",label = h4("Your Likely Time Estimate (hours)"),min = 1, max = 300,1), 
+      br(),
       actionButton("Calculate", label = "submit")    
   
          
     ),
     mainPanel(
       tabsetPanel(
-        tabPanel("BMI Calculation",
+        tabPanel("Expected Time Calculation",
           p(h4("Your Input:")),
-          textOutput("text_height"),
-          textOutput("text_weight"),
-          br(),
-          p(h4("Your BMI Value:")),
-          textOutput("text_BMI"),
-          textOutput("text_status")
-          
+          textOutput("text_optimistic"),
+          textOutput("text_pessimistic"),
+          textOutput("text_likely"),
+          p(h4("Expected Time:")),
+          textOutput("text_expected"),
                  ),
         tabPanel(
           "Documentation",
-          p(h4("Simple BMI Calculator:")),
+          p(h4("Expected Time Calculator:")),
           br(),
-          helpText("This application calculates BMI of person for given height and weight. To calculate your BMI, enter your height and weight in the form"),
-          p(h3("What is BMI?")),
-          helpText("BMI means Body Mass Index. The BMI shows the relation between a person's height and weight, and can be used to indicate whether the person has a normal weight or if he/she is underweight or overweight.,
-          HTML("<u><b>Equation for calculation: </b></u>
-                        <br> <br>
-                        <b> BMI = W /(H * H)</b>
-                        <br>
-                        where: <br>
-                        BMI = Body Mass Index <br>
-                        W = Weight in KG <br>
-                        H = Height in meters <br> ") 
-          
+          helpText("This application calculates the expected time for a task in a Systems Development project, for given optimistic, pessimistic, likely time estimates. Please enter values for your three estimated scenarios in the form"),
                  )
         
         )
