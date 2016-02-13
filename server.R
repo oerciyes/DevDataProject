7ec1c74ecb0a5d2ba23b9602a8140c4cf72730c0
oerciyes <oerciyes@hotmail.com>
2016-02-12 20:39:53 -0600

    Update server.R

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ library(shiny)
 
 Expected<-function(optimistic,pessimistic,likely) {(optimistic+pessimistic+(4*likely))/6}
 
-diagnostic_f<-function(optimistic,pessimistic,likely){
+PERT_f<-function(optimistic,pessimistic,likely){
   Expected_value<-(optimistic+pessimistic+(4*likely))/6
 }
 
@@ -13,7 +13,7 @@ shinyServer(
     output$inputpessimisticvalue <- renderPrint({input$pessimistic})
     output$inputlikelyvalue <- renderPrint({input$likely})
     output$estimation <- renderPrint({Expected(input$optimistic,input$pessimistic,input$likely)})
-    output$diagnostic <- renderPrint({diagnostic_f(input$optimistic,input$pessimistic,input$likely)})
+    output$PERT <- renderPrint({PERT_f(input$optimistic,input$pessimistic,input$likely)})
     
   } 
 )
